295c770a0b8099c43fb183b05f13aa1537abcd25
Geert Van Laethem <geert_van_laethem@peopleware.be>
2018-07-26 14:21:29 +0200

    working on the call expression

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"github.com/geertvl/donkey/token"
 	"bytes"
+	"strings"
 )
 
 type Node interface {
@@ -275,3 +276,26 @@ func (bs *BlockStatement) String() string {
 
 func (bs *BlockStatement) statementNode() {}
 
+type CallExpression struct {
+	Token 		token.Token
+	Function	Expression
+	Arguments	[]Expression
+}
+
+func (ce *CallExpression) expressionNode() {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
